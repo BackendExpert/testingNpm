50e336c72c8babee71e33e950b8efecb7e345491
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 20:08:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- testingnps/src/App.jsx
+++ testingnps/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NoAxios from 'no-axios'
+import { useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // URL of the endpoint
+  const url = 'http://localhost:8081/NewData';
+
+  // Data to be sent in the POST request
+  const data = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 30
+  };
+
+  // const hi = NoAxios.SendToBackEnd(url, data)
+  axios.post(url, data)
+  // console.log("hiiiiiii")
+  .then(response => {
+    // Handle success
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    // Handle error
+    console.error('Errorss:', error);
+  });
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=""></div>
   )
 }
 
